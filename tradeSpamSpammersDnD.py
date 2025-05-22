21d27fa4c63b426b864974c2d74ecc2264691323
Sanoras <sanoras2373@gmail.com>
2025-05-22 02:06:10 -0500

    Update tradeSpamSpammersDnD.py
    added github link in code for credit.

--- tradeSpamSpammersDnD.py
+++ tradeSpamSpammersDnD.py
@@ -3,6 +3,7 @@ import threading
 import time
 import pytesseract
 import random
+#https://github.com/Sanoras1/tradeSpamSpammersDnD/tree/main
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 pyautogui.FAILSAFE = True
