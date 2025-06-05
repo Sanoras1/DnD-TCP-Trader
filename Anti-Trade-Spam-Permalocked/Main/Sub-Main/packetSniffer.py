55b8ea1575e9685e25b2a143f7bc18dad2d28dae
Sanoras <sanoras2373@gmail.com>
2025-06-05 05:59:01 -0500

    Fix myCondition type and update conditional

--- Anti-Trade-Spam-Permalocked/Main/Sub-Main/packetSniffer.py
+++ Anti-Trade-Spam-Permalocked/Main/Sub-Main/packetSniffer.py
@@ -1,7 +1,7 @@
 from scapy.all import sniff, Raw, IP
 
 
-myCondition = ["ip": "52.223.44.23"]
+myCondition = {"ip": "52.223.44.23"}
 "todo: determine min/max, and condition to be considered spammer| work on the actual coding bit for determining when you are traded after entering trade"
 def detect_spam_trade(interface="Ethernet 2", min_len=100, max_len=200, hex_pattern=b'\x68\x65\x6c\x6c\x6f'):
     """
@@ -33,5 +33,5 @@ def detect_spam_trade(interface="Ethernet 2", min_len=100, max_len=200, hex_patt
     sniff(iface=interface, prn=packet_callback, filter="tcp", count=50)
 
 # Call the function with your custom conditions
-if myCondition == True:
+if "ip" in myCondition:
     detect_spam_trade()
